45cfdffe077d8bd41a524f410ba00f6d764f93e2
Emmanuel DE SAINT STEBAN <emmanuel@desaintsteban.fr>
2020-04-19 23:12:44 +0200

    feat(with-server-demo): use NgEntityService

--- src/app/with-server/with-server/state/photos.service.ts
+++ src/app/with-server/with-server/state/photos.service.ts
@@ -1,21 +1,20 @@
 import { Injectable } from '@angular/core';
 import { PhotosStore, PhotosState } from './photos-store.service';
+import {NgEntityService} from '@datorama/akita-ng-entity-service';
 import {PhotosQuery} from './users-query.service';
-import {HttpClient} from '@angular/common/http';
-import {tap} from 'rxjs/operators';
 
 @Injectable({ providedIn: 'root' })
-export class PhotosService  {
+export class PhotosService extends NgEntityService<PhotosState> {
 
 
-  constructor(protected store: PhotosStore, protected query: PhotosQuery, private http: HttpClient) {
-
+  constructor(protected store: PhotosStore, protected query: PhotosQuery) {
+    super(store);
   }
 
-  get(params: {
+  /*get(params: {
     [param: string]: string | string[];
   }) {
     return this.http.get(`https://jsonplaceholder.typicode.com/photos`, {params});
-  }
+  }*/
 
 }
