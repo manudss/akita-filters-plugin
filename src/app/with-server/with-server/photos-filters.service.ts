ae992297a512660bafbaa4107dcaa0e39857a557
manudss <emmanueldesaintsteban@gmail.com>
2020-11-18 18:02:04 +0100

    ✨ feat(akitaMatDataSource): add total in demo page

--- src/app/with-server/with-server/photos-filters.service.ts
+++ src/app/with-server/with-server/photos-filters.service.ts
@@ -2,13 +2,20 @@ import {Injectable} from '@angular/core';
 import {PhotosState} from './state/photos-store.service';
 import {AkitaFiltersPlugin} from 'akita-filters-plugin';
 import {PhotosQuery} from './state/users-query.service';
-import {HashMap} from '@datorama/akita';
+import {HashMap, PaginationResponse} from '@datorama/akita';
 import {PhotosService} from './state/photos.service';
 import {map, tap} from 'rxjs/operators';
+import {PhotosModel} from './state/photosModel';
+import {HttpResponse} from '@angular/common/http';
+import {BehaviorSubject, Observable} from 'rxjs';
 
 
 @Injectable({providedIn: 'root'})
 export class PhotosFiltersService extends AkitaFiltersPlugin<PhotosState> {
+  get total$(): Observable<number> {
+    return this._total$.asObservable();
+  }
+  private _total$ = new BehaviorSubject<number>(0);
 
   constructor(protected query: PhotosQuery, protected photosApi: PhotosService) {
     super(query);
@@ -22,7 +29,14 @@ export class PhotosFiltersService extends AkitaFiltersPlugin<PhotosState> {
       if (filtersObject?._page) {
         filtersObject._page = filtersObject._page + 1;
       }
-      return this.photosApi.get({params: filtersObject});
+      return this.photosApi.get({params: filtersObject,
+        mapResponseFn: (res: HttpResponse<PhotosModel[]>): PhotosModel[] => {
+          this._total$.next(Number(res.headers.get('x-total-count')));
+
+          return res.body;
+        },
+          observe: 'response', // <----- this
+      });
     };
   }
 }
